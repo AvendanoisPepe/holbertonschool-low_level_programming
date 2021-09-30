9717c303c51367bbc9cfb0da997ad63ba01e0223
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-09-30 18:53:18 +0000

    punto 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*the main function prints : "Programming is like building a multilingual puzzle
-*if there is no error, it returns 0
+*main - the function prints : "Programming is like building a multilingual puzzle
+*Return: 0 if there is no error
 */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
