36d4c293bb72867577706db3bb5b7ea3fb552a07
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-09-30 19:13:59 +0000

    punto 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-*main-the function prints :with proper grammar, but the outcome is a piece of art,
+*main-the function prints :with proper grammar,
+*but the outcome is a piece of art,
 *Return:0 if there is no error
 */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
