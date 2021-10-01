4c4a6f3cd3564ddba60026c8dc2eff4fcc9cfcd8
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-01 23:04:12 +0000

    punto 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,27 +5,27 @@
 */
 int main(void)
 {
-  int primero;
-  int segundo;
-  for (primero = 0; primero < 100; primero++)
-    {
-      for (segundo = 0; segundo < 100; segundo++)
-	{
-	  if (primero < segundo)
-	    {
-	      putchar((primero / 10) + 48);
-	      putchar((primero % 10) + 48);
-	      putchar(' ');
-	      putchar((segundo / 10) + 48);
-	      putchar((segundo % 10) + 48);
-	      if (primero != 98 || segundo != 99)
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
-  	}
-    }
-  putchar('\n');
-  return (0);
+int primero;
+int segundo;
+for (primero = 0; primero < 100; primero++)
+{
+for (segundo = 0; segundo < 100; segundo++)
+{
+if (primero < segundo)
+{
+putchar((primero / 10) + 48);
+putchar((primero % 10) + 48);
+putchar(' ');
+putchar((segundo / 10) + 48);
+putchar((segundo % 10) + 48);
+if (primero != 98 || segundo != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
