c133e1dc8d9ec8c224ea254b5ad9dec23d84389a
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-01 17:34:51 +0000

    punto 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main-prints the alphabet except q and c
+*main-prints the alphabet except q and e
 *Return:0 if there is no error
 */
 int main(void)
@@ -8,7 +8,7 @@ int main(void)
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'q' && letter != 'c')
+if (letter != 'q' && letter != 'e')
 {
 putchar(letter);
 }
