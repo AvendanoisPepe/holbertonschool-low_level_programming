2bfd540f93cd2e49d7f80226ebb94977190714c0
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-01 21:32:57 +0000

    punto 10

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,29 +5,29 @@
 */
 int main(void)
 {
-  int primero;
-  int segundo;
-  int tercero;
-  for (primero = 48; primero < 58; primero++)
-    {
-      for (segundo = 49; segundo < 58; segundo++)
-	{
-	  for (tercero = 50; tercero < 58; tercero++)
-	    {
-	      if (tercero > segundo && segundo > primero)
-		{
-		  putchar(primero);
-		  putchar(segundo);
-		  putchar(tercero);
-		  if (primero != 55 || segundo != 56)
-		    {
-		      putchar(',');
-		      putchar(' ');
-		    }
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int primero;
+int segundo;
+int tercero;
+for (primero = 48; primero < 58; primero++)
+{
+for (segundo = 49; segundo < 58; segundo++)
+{
+for (tercero = 50; tercero < 58; tercero++)
+{
+if (tercero > segundo && segundo > primero)
+{
+putchar(primero);
+putchar(segundo);
+putchar(tercero);
+if (primero != 55 || segundo != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
