954b9e372601e27acef8dacddcfbe60696a4c4d6
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-05 05:49:30 +0000

    punto cero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *main-prints the required phrase
- *Return:0 if there is no error
- */
+*main-prints the required phrase
+*Return:0 if there is no error
+*/
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
