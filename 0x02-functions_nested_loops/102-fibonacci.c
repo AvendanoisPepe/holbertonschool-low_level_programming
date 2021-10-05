9d0814ce668cd3b4c32cadfb9e9d81d60db79c9a
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-05 23:02:36 +0000

    imprime los numero Fibonacci hasta el 50

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 /**
+*prints Fibonacci numbers up to 50.
+*Return:0 if there is no error
 */
 int main(void)
 {
- long int contador = 1;
- long int primero = 1;
- long int segundo = 2;
- long int numero;
+long int contador = 1;
+long int primero = 1;
+long int segundo = 2;
+long int numero;
 
- while (contador <= 50)
-   {
-     if (primero != 20365011074)
-       {
-	 printf("%ld, ", primero);
-       }
-     else
-       {
-	 printf("%ld\n", primero);
-       }
+while (contador <= 50)
+{
+if (primero != 20365011074)
+{
+printf("%ld, ", primero);
+}
+else
+{
+printf("%ld\n", primero);
+}
 
-     numero = primero + segundo;
+numero = primero + segundo;
 
-     primero = segundo;
+primero = segundo;
 
-     segundo = numero;
+segundo = numero;
 
-     contador++;
-   }
- return (0);
+contador++;
+}
+return (0);
 }
