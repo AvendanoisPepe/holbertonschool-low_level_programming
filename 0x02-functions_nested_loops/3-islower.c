9b2561d116dd7ce4ccee06ad9b87af60617430bd
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-05 06:01:55 +0000

    punto tres

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *_islower-search for lowercase characters
-*c-is the required character.
+*@c-is the required character.
 *Return:0 if there is no error
 */
 int _islower(int c)
