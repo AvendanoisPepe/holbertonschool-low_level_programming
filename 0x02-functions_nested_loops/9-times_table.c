0b5021f95f79b536159830a33a5131a1c4e1e256
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-05 17:18:31 +0000

    noveno punto

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+*times_table-prints the table of nine
+*/
+void times_table(void)
+{
+int num1 = 0;
+int num2 = 0;
+int num3;
+
+while (num1 < 10)
+{
+while (num2 < 10)
+{
+num3 = num2 * num1;
+if (num2 == 0)
+{
+_putchar(num3 + '0');
+}
+
+if (num3 < 10 && num2 != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(num3 + '0');
+}
+else if (num3 >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((num3 / 10) + '0');
+_putchar((num3 % 10) + '0');
+}
+num2++;
+}
+num1++;
+_putchar('\n');
+}
+}
