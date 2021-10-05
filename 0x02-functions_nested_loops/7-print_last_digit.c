878c0769286718fc6cbdbb3b73754afe8ce29149
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-05 07:07:19 +0000

    septimo punto

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,17 @@
 */
 int print_last_digit(int variable1)
 {
-  int variable2;
-  if (variable1 < 0)
-    {
-      variable1 = -variable1;
-    }
-  variable2 = variable1 % 10;
+int variable2;
+if (variable1 < 0)
+{
+variable1 = -variable1;
+}
+variable2 = variable1 % 10;
   
-  if (variable2 < 0)
-    {
-      variable2 = -variable2;
-    }
-  _putchar(variable2 + '0');
-  return (variable2);
+if (variable2 < 0)
+{
+variable2 = -variable2;
+}
+_putchar(variable2 + '0');
+return (variable2);
 }
