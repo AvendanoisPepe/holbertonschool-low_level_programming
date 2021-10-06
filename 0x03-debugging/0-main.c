8d0cc671f84e5bf6f0be9c61f5a0e348ded1966b
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-06 17:43:10 +0000

    veamos a ver si falla

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,16 @@
 #include "main.h"
+
 /**
-*main-decides whether it is negative or positive
-*Return:0 if there is no error
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
 int main(void)
 {
-int number = 0;
-positive_or_negative(number);
-return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
