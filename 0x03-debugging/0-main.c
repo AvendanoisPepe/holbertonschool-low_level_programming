8adb58cc14ebc63047a751603755a2a72a9b4e0a
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-06 15:16:51 +0000

    no creo que sirva

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+*/
+int main(void)
+{
+  int number= 0;
+  positive_or_negative(number);
+  return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int positive_or_negative(int);
+#endif
