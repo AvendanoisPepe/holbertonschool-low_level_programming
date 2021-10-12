6fb38faef32f56b6189a985504d03beda1656681
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-12 01:04:52 +0000

    genera contraseñas aleatorias para el programa 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,15 +16,15 @@ int main(void)
 
 	while (variable1 < 100)
 	{
-		clave[variable1] = rand() % 78;
+		clave[variable1] = rand() % 50;
 
 		suma += (clave[variable1] + '0');
 
 		putchar(clave[variable1] + '0');
 
-		if ((2772 - suma) - '0' < 78)
+		if ((3214 - suma) - '0' < 50)
 		{
-			variable2 = 2772 - suma - '0';
+			variable2 = 3214 - suma - '0';
 			suma += variable2;
 			putchar(variable2 + '0');
 			break;
