425b598b8e3edf3b2c16e74c1b6232f52f61f520
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-14 18:03:32 +0000

    Convierte en mayusculas todas las iniciales de las palabras de la cadena y creo mi propio array para las separaciones

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,21 +11,20 @@ char *cap_string(char *w)
 	int variable = 0;
 	int variable2 = 0;
 
-        if (variable == 0 && w[variable] >= 'a' && w[variable] <= 'z')
-	{
-		w[variable] = -32;
-	}
-	
 	while (w[variable] != '\0')
 	{
-
+		if (variable == 0 && w[variable] >= 'a' && w[variable] <= 'z')
+		{
+			w[variable] -= 32;
+		}
+		variable2 = 0;
 		while (variable2 < 13)
 		{
 			if (w[variable] == arreglo[variable2])
 			{
 				if (w[variable + 1] >= 'a' && w[variable + 1] <= 'z')
 				{
-					w[variable] = -32;
+					w[variable + 1] -= 32;
 				}
 			}
 
