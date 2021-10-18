bc5436b273f3f7234a59bcea8cef32c3aade6cd1
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-18 20:06:58 +0000

    Se rellena con el contador el array osea s usando el limite de n y reemplazando con b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  *  *_memset-The memory is filled with a constant byte.
- * @s: Espacio de memorio a completar o rellenar. 
+ * @s: Espacio de memorio a completar o rellenar.
  * @b:char que se debe copiar.
  * @n:el numero de bytes que se copea b.
+ * Return:el puntero al s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int datito = 0;
+unsigned int datito = 0;
 
-    while (datito < n)
-    {
-        s[datito] = b;
-        datito++;
-    }
-    return (s);
+while (datito < n)
+{
+s[datito] = b;
+datito++;
+}
+return (s);
 }
