7e7874c403897129d4ba720cd5cc5c1f22eec858
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-22 17:01:06 +0000

    cambia el nombre del archivo por medio de los parametros

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,10 @@
  * main-cambia el nombre del archivo
  * @argc:numero de argumentos que se le pasan al programa.
  * @argv:argumentos en array.
+ * Return: finalizo bien si es 0 .
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s \n", *argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
