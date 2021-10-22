747bdaad0c606c9aca89236a8a30ad5a6c793e58
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-22 17:46:49 +0000

    imprime todos los argumentos que recibe

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main-imprime todos los argumentos que recibe
+ *main-imprime todos los argumentos que recibe
  * @argc:numero de argumentos que se le pasan al programa.
  * @argv:argumentos en array.
  * Return: finalizo bien si es 0 .
 */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-        	printf("%s\n", argv[i]);
-    	}
-    	return (0);
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
