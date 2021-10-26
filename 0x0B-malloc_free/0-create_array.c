0aec062c212848de0a19339edaa3fed8563123ed
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-26 17:35:20 +0000

    Creo un matriz y le asigno memoria dinamica con malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *  *create_array-Se crea la matriz guardarT y la inicializo con c,
+ * @size:Tamaño de la matriz.
+ * @c:caracter que la inicializa.
+ * Return:Puntero a la matriz si se logra, si no null.
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *guardarT;
+	unsigned int iterador = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	guardarT = malloc(sizeof(char) * size);
+
+	while (iterador < size)
+	{
+		guardarT[iterador] = c;
+		iterador++;
+	}
+
+	return (guardarT);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+#lo hago junto con los dos anteriores
