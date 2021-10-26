2e59e1718fcae324d2a0422c3fa8acec2ef5b248
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-26 21:31:19 +0000

    Concateno dos cadenas mandadas por parametros en el cual encontramos la longitud de bytes de cada uno y con las mismas usamos malloc para tener el espacio en memoria para al final unir la 1 con la 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *str_concat-Concatenamos las dos cadenas pasadas por parametro.
+ * @s1:Primera cadena.
+ * @s2:Segunda cadena.
+ * Return:Puntera a la nueva cadena concatenada y si falla null.
+*/
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int longitud1 = 0;
+	unsigned int longitud2 = 0;
+	char *ss;
+	unsigned int iterador1;
+	unsigned int iterador2;
+
+	while (s1[longitud1])
+	{
+		longitud1++;
+	}
+
+	while (s2[longitud2])
+	{
+		longitud2++;
+	}
+
+	ss = malloc(sizeof(char) * (longitud1 + longitud2 + 1));
+
+	if (ss == NULL)
+	{
+		return (NULL);
+	}
+	if (s1)
+	{
+		for (iterador1 = 0; iterador1 < longitud1; iterador1++)
+		{
+			ss[iterador1] = s1[iterador1];
+		}
+	}
+	if (s2)
+	{
+		for (iterador2 = 0; iterador2 < (longitud1 + longitud2); iterador2++)
+		{
+			ss[iterador1] = s2[iterador2];
+			iterador1++;
+		}
+	}
+	ss[iterador1] = '\0';
+	return (ss);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,5 @@
 0-create_array.c = Creo un matriz y le asigno memoria dinamica con malloc.
 
 1-strdup.c =  Copia la cadena str recorriendo el tamaño de la misma y obteniendo asi el tamaño de memoria con el malloc, seguido comparamos las dos cadenas para verificar
+
+2-str_concat.c = 
