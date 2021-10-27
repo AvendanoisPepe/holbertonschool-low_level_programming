76da29f754378d63b1c22437e03561a56bb6473f
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-27 04:11:01 +0000

    En simples palabras generamos dos malloc para cada eje y a los dos los recorremos poniendo condicionales como si son nulos y si es el caso que libere el espacio en memoria

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *alloc_grid-Creamos una cuadricula de 2 dimenciones con malloc
+ * @width:Ancho de la cuadricula.
+ * @height:alto de la cuadricula,
+ * Return:Puntero a cuadricula.
+*/
+int **alloc_grid(int width, int height)
+{
+	int **guardarT;
+	int iterador;
+	int iterador2;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	guardarT = malloc(sizeof(int *) * height);
+
+	if (guardarT == NULL)
+		return (NULL);
+
+	for (iterador = 0; iterador < height; iterador++)
+	{
+		guardarT[iterador] = malloc(sizeof(int **) * width);
+
+		if (guardarT[iterador] == NULL)
+		{
+			for (; iterador >= 0; iterador--)
+			{
+				free(guardarT[iterador]);
+			}
+			free(guardarT);
+			return (NULL);
+		}
+
+		for (iterador2 = 0; iterador2 <  width; iterador2++)
+		{
+			guardarT[iterador][iterador2] = 0;
+		}
+	}
+	return (guardarT);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -5,3 +5,5 @@
 1-strdup.c =  Copia la cadena str recorriendo el tamaño de la misma y obteniendo asi el tamaño de memoria con el malloc, seguido comparamos las dos cadenas para verificar
 
 2-str_concat.c = Concateno dos cadenas mandadas por parametros en el cual encontramos la longitud de bytes de cada uno y con las mismas usamos malloc para tener el espacio en memoria para al final unir la 1 con la 2.
+
+3-alloc_grid.c = 
