5ab6cec4b5c1cbf3b0dd4a07cfce79df1eba42c1
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-28 20:57:23 +0000

    Concateno dos cadenas, la cadena origen es s1 y concatenamos el numero n de bytes de la cadena s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			longitud2++;
 		}
-	if (n <= longitud2)
+	if (n <longitud2)
 		guardarT = malloc(sizeof(char) * (longitud1 + n + 1));
 	else
 		guardarT = malloc(sizeof(char) * (longitud1 + longitud2 + 1));
@@ -36,14 +36,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (; iterador1 < longitud1; iterador1++)
 		guardarT[iterador1] = s1[iterador1];
-	for (; n < longitud2 && iterador1 < (longitud1 + n); iterador1++)
-	{
-		guardarT[iterador1] = s2[iterador2++];
-	}
-	for (; n >= longitud2 && iterador1 < (longitud1 + longitud2); iterador1++)
-	{
-		guardarT[iterador1] = s2[iterador2++];
-	}
+	if (n < longitud2)
+		for (; iterador1 < (longitud1 + n); iterador1++)
+			guardarT[iterador1] = s2[iterador2++];
+	else
+		for (; n >= longitud2 && iterador1 < (longitud1 + longitud2); iterador1++)
+			guardarT[iterador1] = s2[iterador2++];
 	guardarT[iterador1] = '\0';
 	return (guardarT);
 }
