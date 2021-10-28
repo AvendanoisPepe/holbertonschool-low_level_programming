79f1d66103f2e384cb1cc7ac959f7f3c205f84d3
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-28 23:49:51 +0000

    En la primera funcion reservo el espacio de memoria con el malloc y en la segunda la recorro para llenar la memoria.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * *probando-Ingresar o llenar la memoria reservada.
+ * @guardar:malloc pasado por parametro.
+ * @add:Inicializador.
+ * @byt: numero de veces que recorre a base de la mul de los
+ * parametros de la otra funcion.
+ * Return:puntero a guardar.
+*/
+char *probando(char *guardar, char add, unsigned int byt)
+{
+	unsigned int iterador = 0;
+
+	while (iterador < byt)
+	{
+		guardar[iterador] = add;
+		byt++;
+	}
+	return (guardar);
+}
+/**
  * *_calloc-Con malloc se asigna la memoria a la matrix,
  * @nmemb:elementos de la matrix.
  * @size:tamaño de cada elemento.
@@ -25,24 +44,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	probando(guardarT, 0, nmemb * size);
 
 	return (guardarT);
-}
-
-/**
- * *probando-Ingresar o llenar la memoria reservada.
- * @guardar:malloc pasado por parametro.
- * @add:Inicializador.
- * @byt: numero de veces que recorre a base de la mul de los
- * parametros de la otra funcion.
- * Return:puntero a guardar.
-*/
-char *probando(char *guardar, char add, unsigned int byt)
-{
-	unsigned int iterador = 0;
-
-	while (iterador < byt)
-	{
-		guardar[iterador] = add;
-		byt++;
-	}
-	return (guardar);
 }
