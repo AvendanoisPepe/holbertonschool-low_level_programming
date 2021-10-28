a6e9a2974da05fdf72d8c03d5cd36f5b5c8809c0
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-10-28 18:58:30 +0000

    Concateno dos cadenas, la cadena origen es s1 y concatenamos el numero n de bytes de la cadena s2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *string_nconcat-Concatena n bytes de s2 a s1
+ * @s1:primera cadena-recibe
+ * @s2:segunda cadena-da
+ * @n:numero de bytes a concatenera.
+ * Return:Puntero a la cadena.
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int longitud1 = 0, longitud2 = 0, iterador1, iterador2;
+	char *guardarT;
+
+	if (s1 == NULL)
+		longitud1 = 0;
+	else
+		while (s1[longitud1])
+		{
+			longitud1++;
+		}
+	if (s2 == NULL)
+		longitud2 = 0;
+	else
+		while (s2[longitud2])
+		{
+			longitud2++;
+		}
+	if (n < longitud2)
+		guardarT = malloc(sizeof(char) * (longitud1 + n + 1));
+	else
+		guardarT = malloc(sizeof(char) * (longitud1 + longitud2 + 1));
+	iterador2 = 0;
+	iterador1 = 0;
+	if (guardarT == NULL)
+		return (NULL);
+	for (; iterador1 < longitud1; iterador1++)
+		guardarT[iterador1] = s1[iterador1];
+	for (; n < longitud2 && iterador1 < (longitud1 + n); iterador1++)
+	{
+		guardarT[iterador1] = s2[iterador2++];
+	}
+	for (; n >= longitud2 && iterador1 < (longitud1 + longitud2); iterador1++)
+	{
+		guardarT[iterador1] = s2[iterador2++];
+	}
+	guardarT[iterador1] = '\0';
+	return (guardarT);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,6 @@
 #pribando
 
 0-malloc_checked.c = Asignando el paremetro a la funcion malloc obtenemos el espacio de memoria y si el mismo falla lo cierra forzosamente con exit retornando 98.
+
+1-string_nconcat.c = Concateno dos cadenas, la cadena origen es s1 y concatenamos el numero n de bytes de la cadena s.
+
