55ed8cac9e20c6a1c51abaa0edf5ec046471e82e
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-11-02 00:52:26 +0000

    Imprime cierta estructura sobre los datos de un perro.

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - Imprime una estructura sobre datos de un perro.
+ * @d:Puntero a la estructura.
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "nill";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "nill";
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -3,3 +3,6 @@
 dog.h = Creo la estructura basica de un perro.
 
 1-init_dog.c =  Inicializo la estructura creada en el punto anterior.
+
+2-print_dog.c = Imprime cierta estructura sobre los datos de un perro.
+
