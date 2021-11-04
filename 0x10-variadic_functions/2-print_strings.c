53f1711e36dc6bbfc8052c9f7006b9a229130921
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-11-04 18:32:32 +0000

    Imprimo dos cadenas conjuntas pasadas por parametros con un separador entre cadenas tambien pasado por parametro.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("(nill)");
+			printf("(nil)");
 		}
 		if (iterador < (n - 1) && separator != NULL)
 		{
