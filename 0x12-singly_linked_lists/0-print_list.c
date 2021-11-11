905c3626e82acbc0d9d9c642a10c35cf24fe7276
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-11-11 18:03:08 +0000

    Imprime todos los elementos de la lista list_t

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - Imprime todos los elementos de la lista.
+ * @h:Puntero a la lista a imprimir.
+ * Return:iterador;
+ */
+size_t print_list(const list_t *h)
+{
+	int iterador = 0;
+
+	for (; iterador < h; iterador++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (iterador);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+#julio verne :v
+
+0-print_list.c - Imprime todos los elementos de la lista list_t
