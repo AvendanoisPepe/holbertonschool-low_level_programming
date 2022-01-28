225945a392a8ebd2d55027cdff02ee8a75df5bdb
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-01-28 19:18:56 +0000

    Sixth point

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,23 +1,4 @@
 #include "hash_tables.h"
-
-/**
- * free_list - frees a linked list
- * @head: hash_node_t list to be freed
- */
-void free_list(hash_node_t *head)
-{
-	hash_node_t *temp;
-
-	while (head)
-	{
-		temp = head->next;
-		free(head->key);
-		free(head->value);
-		free(head);
-		head = temp;
-	}
-}
-
 /**
  * hash_table_delete - Libera la tabla hash
  *
@@ -25,13 +6,27 @@ void free_list(hash_node_t *head)
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
+	hash_node_t *temporal = NULL;
+	unsigned long int index = 0;
 
-	if (!ht)
-		return;
-
-	for (i = 0; i < ht->size; i++)
-		free_list(ht->array[i]);
-	free(ht->array);
-	free(ht);
+	if (ht)
+	{
+		while (index < ht->size)
+		{
+			while (ht->array[index])
+			{
+				temporal = ht->array[index];
+				ht->array[index] = ht->array[index]->next;
+				if (temporal->value)
+					free(temporal->value);
+				if (temporal->key)
+					free(temporal->key);
+				free(temporal);
+			}
+			free(ht->array[index]);
+			index++;
+		}
+		free(ht->array);
+		free(ht);
+	}
 }
