e4af5525589aad4f9a36b526d4187dae8243adc4
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-01-28 22:23:26 +0000

    Sixth point

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,9 +9,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *nuevo = NULL, *temporal = NULL;
+	hash_node_t *nuevo, *temporal;
 	unsigned long int index;
 
+	nuevo = malloc(sizeof(hash_node_t));
+	if (nuevo == NULL)
+		return (0);
 	if (value == NULL || key == NULL || ht == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
@@ -23,17 +26,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(temporal->value);
 			temporal->value = strdup(value);
 			if (temporal->value != NULL)
+			{
 				return (1);
+			}
 			return (0);
 		}
 		temporal = temporal->next;
 	}
-	nuevo = ht->array[index];
 	nuevo->key = strdup(key);
 	nuevo->value = strdup(value);
-	nuevo = malloc(sizeof(hash_node_t));
-	if (nuevo == NULL)
-		return (0);
 	if (nuevo->key == NULL)
 	{
 		free(nuevo);
